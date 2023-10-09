2c39df921ffe646e1dc521e9f125beb17feb825b
Riccardo <riky.volto@live.com>
2023-10-09 17:31:38 +0200

    print *

--- script.php
+++ script.php
@@ -4,6 +4,9 @@ var_dump($_GET);
 
 $paragraph = $_GET['paragraph'];
 $censured_text = $_GET['censured_text'];
+$censured_text_lenght = strlen($censured_text);
+$censured_world = substr_replace($censured_text, str_repeat('*', $censured_text_lenght), $censured_text_lenght);
+
 
 
 
@@ -15,6 +18,7 @@ $censured_text = $_GET['censured_text'];
 <body>
    paragrafo: <p><?php echo $_GET["paragraph"]?> <?php echo strlen($paragraph)?></p>
    parola censurata: <p><?php echo $_GET["censured_text"]?> <?php echo strlen($censured_text)?></p>
+   paragrafo con la parola censurata: <p><?php echo $censured_world ?></p>
 
     
 </body>
